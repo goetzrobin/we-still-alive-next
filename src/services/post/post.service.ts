74e353da68a1d8b295c2cf51b6dbf4c82498b627
Robin Goetz <tug29225@temple.edu>
2021-12-02 17:17:28 -0500

    setting page size to a lot more

--- src/services/post/post.service.ts
+++ src/services/post/post.service.ts
@@ -6,7 +6,7 @@ import Prismic from 'prismic-javascript';
 import ImageService from '@services/image/image.service';
 
 const fetchPosts = async (
-  pageSize = 10,
+  pageSize = 1000,
   page = 1,
   maxTags = 100
 ): Promise<PostsPreviewData | null> => {
